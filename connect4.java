c2e9d24fb8e08d103c647c71016ff9858bb1caf6
BrianHSTA <brian.huang26@ycdsbk12.ca>
2025-06-03 11:02:24 -0400

    Beginning on other features

--- connect4.java
+++ connect4.java
@@ -1,47 +1,28 @@
 import arc.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.Font;
+
 
 public class connect4{
 	public static void main(String[] args){
-	Console con = new Console();
-	//[x][y]
-	int intBoard[][];
-	intBoard = new int[7][6];
-	
-	int intPieceCoordinate[];
-	intPieceCoordinate = new int[2];
-	
-	int intMove = 0;
-	int intPlayer = 1;
-	int intNewBoard[][];
-	int intMoveCount = 0;
-
-	while (intMoveCount <= 42){
-	
-		intMove = con.readInt();
-		intMove--;
-		intPieceCoordinate = connect4methods.intPieceCoordinate(intBoard, intMove, intPlayer);
 		
-		int intTestCount;
-		for (intTestCount = 0; intTestCount <= 6; intTestCount++){
-			con.println(intBoard[intTestCount][0]+" - "+intBoard[intTestCount][1]+" - "+intBoard[intTestCount][2]+" - "+intBoard[intTestCount][3]+" - "+intBoard[intTestCount][4]+" - "+intBoard[intTestCount][5]);
-		}
-
-		//con.println(intNewBoard[intMove][0]);
-		//con.println(intNewBoard[intMove][1]);
-		//con.println(intNewBoard[intMove][2]);
-		//con.println(intNewBoard[intMove][3]);
-		//con.println(intNewBoard[intMove][4]);
-		//con.println(intNewBoard[intMove][5]);
+		TextInputFile lasttheme = new TextInputFile("lasttheme.txt")
+		String strThemeName = lasttheme.readline();
+		String strPlayer1RGB = lasttheme.readline();
+		String strPlayer2RGB = lasttheme.readline();
+		String strBoardColor = lasttheme.readline();
+		String strTitle = lasttheme.readline();
 		
-		if (intPieceCoordinate[0] == -1 && intPieceCoordinate[1] == -1){
-			con.println("Invalid move, go again");
-		}else{
-			intBoard[intPieceCoordinate[0]][intPieceCoordinate[1]] = intPlayer;
-			intPlayer = intPlayer%2+1;
-			intMoveCount++;
-		}
-	
-	}
+		
+		Console con = new Console("Connect 4", 700,700)
+		// set the background
+		con.setDrawColor(Color.BLACK);
+		con.fillRect(0,0,700,700);
+			
+		
+		
+		
 		
 	}
 }
--- connect4methods.java
+++ connect4methods.java
@@ -2,7 +2,7 @@ import arc.*;
 
 public class connect4methods{
 	
-	public static int[] intPieceCoordinate(int[][] intBoard, int intMove, int intPlayer){
+	public static int[] PieceCoordinate(int[][] intBoard, int intMove, int intPlayer){
 		int intPieceCoordinate[];
 		intPieceCoordinate = new int[2];
 		int intCount = 5;
@@ -25,6 +25,27 @@ public class connect4methods{
 		
 	}
 	
+	public static int[] StrRGBtoIntRGB(String strRGB){
+		int intRGB[];
+		intRGB = new int[3];
+		String strNum;
+		boolean blnRun;
+		
+		int intCount;
+		
+		for (intCount = 0; intCount<3; intCount++){
+			blnRun = true;
+			while (blnRun == true){
+				if (strRGB.substring(0,1)==","){
+					strRGB = strRGB.substring(1,strRGB.length());
+					blnRun = false;
+				}else{
+					strRGB = strRGB.substring(1,strRGB.length());
+				}
+			}
+		}
+	}
 	
 	
 }
+
